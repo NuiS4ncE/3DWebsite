79c2e90cc366efa1d958b716924174b3bbb1d0f9
NuiS4ncE <nuis4nce@hotmail.com>
2023-04-05 01:18:02 +0300

    fooling around

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,41 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+var backgroundColor = 0xCDD3D6;
+
+
+// Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+///background
+renderer.setClearColor(backgroundColor, 1);
+
+
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0x0752FF });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame(animate);
+function render() {
+	
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
 }
 
-animate();
+function Environment() {
+	
+}
+
+render();
 /*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
