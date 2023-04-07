909b4a0f4b08353f83cb4eac775af0df799fd43c
NuiS4ncE <nuis4nce@hotmail.com>
2023-04-08 00:12:17 +0300

    changed website title, added comments for clarity

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import * as THREE from 'three';
 import * as OIMO from 'oimo';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+// Threejs scene
 const scene = new THREE.Scene();
+
+// Camera setup
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(-3, 0, 30);
+camera.position.set(-45, 45, -30);
 scene.add(camera);
 
-// Oimo
+// Oimo world setup
 const world = new OIMO.World({
     timestep: 1 / 60,
     iterations: 8,
@@ -16,7 +19,6 @@ const world = new OIMO.World({
     random: true,
     info: true // display statistique
 });
-
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -28,8 +30,6 @@ document.body.appendChild(renderer.domElement);
 //var backgroundColor = 0xCDD3D6;
 //renderer.setClearColor(backgroundColor, 1);
 
-//var count = 0, sum = 0, average = 0;
-
 // Blue cube
 const geometry = new THREE.BoxGeometry(10, 10, 10);
 const material = new THREE.MeshBasicMaterial({ color: 0x0752FF });
@@ -37,8 +37,8 @@ const cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 20, 0);
 scene.add(cube);
 
-// Cube oimo collider
-var body = world.add({
+// Cube Oimo collider
+const body = world.add({
     type: 'box',
     size: [10, 10, 10],
     pos: [0, 20, 0],
@@ -50,9 +50,6 @@ var body = world.add({
     belongsTo: 1,
     collidesWith: 0xffffffff
 });
-
-
-
 
 // Floor
 const plane = new THREE.PlaneGeometry(1000, 1000, 1, 1);
@@ -66,9 +63,7 @@ floor.rotation.x = - Math.PI / 2;
 floor.receiveShadow = true;
 scene.add(floor);
 
-
 // Floor collider in Oimo
-//const floorShape = new OIMO.BoxShape(1000, 0.1, 1000);
 const ground = world.add({
     type: 'box',
     size: [1000, 1, 1000],
@@ -79,25 +74,11 @@ const ground = world.add({
     restitution: 0.1
 })
 
-// Create a OIMO.Body object for the floor with a static type
-/*const floorBody = new OIMO.Body({
-    type: 'static',
-    position: [0, -0.1, 0], // Set the position slightly below the visual mesh
-    rotation: [0, 0, 0],
-    move: false,
-    shapes: [floorShape]
-});*/
-
-// Add the physics body to the Oimo.js world
-//world.addBody(floorBody);
-
 // Torus
-
 const geometry2 = new THREE.TorusGeometry(10, 3, 16, 100);
 const material2 = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry2, material2);
-torus.position.set(0, 20, 0);
-
+torus.position.set(0, 30, 0);
 scene.add(torus);
 
 // Lights 
@@ -108,29 +89,30 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+// Scene mouse-paning controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function render() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.01;
 
-    if (cube.rotation.x > 360 || cube.rotation.y > 360 || torus.rotation.x > 360 || torus.rotation.y > 360) {
-        cube.rotation.x = 0;
-        cube.rotation.y = 0;
+    if (torus.rotation.x > 360 || torus.rotation.y > 360) {
         torus.rotation.x = 0;
         torus.rotation.y = 0;
     }
+
+    // Update cube mesh position and rotation per rigidbody location
     cube.position.copy(body.getPosition());
     cube.quaternion.copy(body.getQuaternion());
+
+    // Oimo physics render function
     world.step();
+
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
